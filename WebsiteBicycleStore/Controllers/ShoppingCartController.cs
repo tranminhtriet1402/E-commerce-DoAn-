5a7965bb911b55d037be1f9d02fdfbfd7c43099d
tranminhtriet1402 <73575868+tranminhtriet1402@users.noreply.github.com>
2020-12-01 16:34:41 +0700

    Update Paypal

--- WebsiteBicycleStore/Controllers/ShoppingCartController.cs
+++ WebsiteBicycleStore/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Ajax.Utilities;
+using PayPal.Api;
 //using PayPal.Api;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using WebsiteBicycleStore.Models;
+using Order = WebsiteBicycleStore.Models.Order;
 
 namespace WebsiteBicycleStore.Controllers
 {
@@ -45,7 +47,6 @@ namespace WebsiteBicycleStore.Controllers
                 ViewBag.mess = "Bạn chưa thêm sản phẩm nào";
             }
             Cart cart = Session["Cart"] as Cart;
-            db.Users.ToList();
             return View(cart);
         }
 
@@ -145,8 +146,6 @@ namespace WebsiteBicycleStore.Controllers
                 {
                     OrderDetail _order_Detail = new OrderDetail();
                     _order_Detail.IDOrder = _order.IDOrder;
-                    int var = (int)_order_Detail.IDOrder;
-                    Session["mhd"] = var;
                     _order_Detail.ngayDat = _order.OrderDate;
                     _order_Detail.namePro = item._shopping_product.NameProduct;
                     _order_Detail.ngayNhan = DateTime.Now.AddDays(5);
@@ -169,20 +168,139 @@ namespace WebsiteBicycleStore.Controllers
         }
 
 
-        //public ActionResult paypal()
-        //{
-        //    APIContext apicontext = PaypalConfiguration.GetAPIContext();
-        //    try
-        //    {
-        //        string PayerId=Request.Pa
-        //    }
-        //    catch (Exception)
-        //    {
+        private Payment payment;
 
-        //        throw;
-        //    }
+        // Create a paypment using an APIContext
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl)
+        {
+            var listItems = new ItemList() { items = new List<Item>() };
 
-        //}
+            Cart cart = Session["Cart"] as Cart;
+            foreach (var item in cart.Items)
+            {
+                listItems.items.Add(new Item()
+                {
+                    name = item._shopping_product.NameProduct,
+                    currency = "USD",
+                    price = item._shopping_product.UnitPrice.ToString(),
+                    quantity = item._shopping_quantity.ToString(),
+                    sku = "sku"
+                });
+            }
 
+            var payer = new Payer() { payment_method = "paypal" };
+
+            // Do the configuration RedirectURLs here with redirectURLs object
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = redirectUrl,
+                return_url = redirectUrl
+            };
+
+            // Create details object
+            var details = new Details()
+            {
+                tax = "1",
+                shipping = "2",
+                subtotal = cart.Items.Sum(x => x._shopping_quantity * x._shopping_product.UnitPrice).ToString()
+            };
+
+            // Create amount object
+            var amount = new Amount()
+            {
+                currency = "USD",
+                total = (Convert.ToDouble(details.tax) + Convert.ToDouble(details.shipping) + Convert.ToDouble(details.subtotal)).ToString(),// tax + shipping + subtotal
+                details = details
+            };
+
+            // Create transaction
+            var transactionList = new List<Transaction>();
+            transactionList.Add(new Transaction()
+            {
+                description = "Chien Testing transaction description",
+                invoice_number = Convert.ToString((new Random()).Next(100000)),
+                amount = amount,
+                item_list = listItems
+            });
+
+            payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactionList,
+                redirect_urls = redirUrls
+            };
+
+            return payment.Create(apiContext);
+        }
+
+        // Create ExecutePayment method
+        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = payerId
+            };
+            payment = new Payment() { id = paymentId };
+            return payment.Execute(apiContext, paymentExecution);
+        }
+
+        // Create PaymentWithPaypal method
+        public ActionResult PaymentWithPaypal()
+        {
+            // Gettings context from the paypal bases on clientId and clientSecret for payment
+            APIContext apiContext = PaypalConfiguration.GetAPIContext();
+
+            try
+            {
+                string payerId = Request.Params["PayerID"];
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    // Creating a payment
+                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority + "/ShoppingCart/PaymentWithPaypal?";
+                    var guid = Convert.ToString((new Random()).Next(100000));
+                    var createdPayment = CreatePayment(apiContext, baseURI + "guid=" + guid);
+
+                    // Get links returned from paypal response to create call funciton
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = string.Empty;
+
+                    while (links.MoveNext())
+                    {
+                        Links link = links.Current;
+                        if (link.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            paypalRedirectUrl = link.href;
+                        }
+                    }
+
+                    Session.Add(guid, createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    // This one will be executed when we have received all the payment params from previous call
+                    var guid = Request.Params["guid"];
+                    var executedPayment = ExecutePayment(apiContext, payerId, Session[guid] as string);
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        //Remove shopping cart session
+                        //Session.Remove(strCart);
+                        return View("FailureView");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //PaypalLogger.Log("Error: " + ex.Message);
+                //Remove shopping cart session
+                //Session.Remove(strCart);
+                return View("FailureView");
+            }
+
+            //Remove shopping cart session
+            //Session.Remove(strCart);
+            return View("SuccessView");
+        }
     }
 }
--- WebsiteBicycleStore/PaypalConfiguration.cs
+++ WebsiteBicycleStore/PaypalConfiguration.cs
@@ -9,28 +9,28 @@ namespace WebsiteBicycleStore
 {
     public class PaypalConfiguration
     {
-        public readonly static string clientId;
-        public readonly static string clientSecret;
+        public readonly static string ClientId;
+        public readonly static string ClientSecret;
         static PaypalConfiguration()
         {
-            var config = getconfig();
-            clientId = "ActeHjPhCmH5EZcwMUFBupkA0SsX4D1EGAXxy-evSiKfOPkDSdKb546dbOP1Z6-Qsps3jcW7jUGeVa6z";
-            clientSecret = "EFYxlf4o9zXFXJmoevyvQL1-s0giZgroQtOpA6YMoifxuMT9--1HpYVBgj3_r9y6_VrLMme0QpBQuzgi";
+            var config = GetConfig();
+            ClientId = "ActeHjPhCmH5EZcwMUFBupkA0SsX4D1EGAXxy-evSiKfOPkDSdKb546dbOP1Z6-Qsps3jcW7jUGeVa6z";
+            ClientSecret = "ELvj_-30s8K0p2IG_prnZG8CpBHgbTLwQlmIk111TsuAXfxysts2tAflT1eXM2zmRtB7UPNWxGkE_d_E";
         }
 
-        private static Dictionary<string,string> getconfig()
+        private static Dictionary<string,string> GetConfig()
         {
             return PayPal.Api.ConfigManager.Instance.GetProperties();
         }
         private static string GetAccessToken()
         {
-            string accessToken = new OAuthTokenCredential(clientId, clientSecret,getconfig()).GetAccessToken();
+            string accessToken = new OAuthTokenCredential(ClientId, ClientSecret,GetConfig()).GetAccessToken();
             return accessToken;
         }
         public static APIContext GetAPIContext()
         {
             APIContext apicontext = new APIContext(GetAccessToken());
-            apicontext.Config = getconfig();
+            apicontext.Config = GetConfig();
             return apicontext;
         }
     }
